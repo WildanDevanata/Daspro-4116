449e8c05b3c5924736c2f75ac98237e660b3385e
WildanDevanata <112999309+WildanDevanata@users.noreply.github.com>
2022-09-24 20:45:10 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int p,l,luas;
+    cout <<"Masukan panjang persegi panjang :";
+    cin >>p;
+    cout <<"Masukan lebar persegi panjang :";
+    cin >>l;
+
+    luas = p*l;
+    cout <<"Luas persegi panjang adalah :" <<luas; <<endl;
+    return 0;
+}
